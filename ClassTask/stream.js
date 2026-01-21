396b20fa42a630568a81096810fcba3ca373d7fb
Suryansh Kumar Pathak <rahulbuxar0001@gmail.com>
2026-01-21 16:20:27 +0530

    [Suryansh] added class task

---
+++ ClassTask/stream.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const fs = require("fs");
+
+const Stream = path.join(__dirname,"./stream.txt")
+
+const readStream  = fs.createReadStream(Stream,{
+    highWaterMark: 64*1024
+})
+
+
+readStream.on("data",(chunk)=>{
+    console.log(chunk.toString())
+})
+
+readStream.on("end",()=>{
+    console.log("Data Finshed")
+})
+
+
+
